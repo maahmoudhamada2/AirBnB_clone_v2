55ec396be827adf0ffa236cbeb2ca847b363b1a3
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-06 05:38:31 +0300

    Adding: Delete method and specific objs list

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            custom_objs = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    custom_objs.update({key: value})
+            return custom_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Method to delete an obj from FileStorage"""
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                self.__objects.pop(key)
---
+++ tst.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from models import storage
+from models.city import City
+
+
+c = City()
+
+print(c)
