faab68bd16c2f195f24cccc1a42ffb074cd7a3b8
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-08-31 08:19:59 +0300

    Submit: web_flask --> 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""0-hello_route app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hello():
+    """Routing method to return a string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Routing method to return a string"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_function(text):
+    """Routing method to return C string """
+    text = 'C ' + text.replace('_', ' ')
+    return text
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_function(text="is cool"):
+    """Routing method to return python string"""
+    text = 'Python ' + text.replace('_', ' ')
+    return text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
